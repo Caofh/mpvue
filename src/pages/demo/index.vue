30b34cbe1c54f37c2554935f3217e59e5844d06c
caofanghui <caofanghui@secoo.com>
2019-04-16 23:26:53 +0800

    update-version1.

---
+++ src/pages/demo/index.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <div @click="click">
+      牛逼啦
+    </div>
+
+    <div class="content">
+      引个图片
+      <img class="img" :src="img">
+      <img class="img" :src="invite">
+    </div>
+
+    <div class="bg">
+
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      motto: 'Hello miniprograme',
+      img: require('../../assets/img/root_demo_demo.jpeg'),
+      invite: require('../../assets/img/root_demo_invite.png'),
+
+    }
+  },
+
+  components: {
+
+  },
+
+  methods: {
+      click () {
+          wx.showModal({
+              title: '提示',
+              showCancel: false,
+              content: '陈宫1235',
+              confirmText: '确定',
+              success: res => {
+                  if (res.confirm) {
+                      console.log(123)
+                  }
+              }
+          })
+
+
+
+      }
+
+  },
+
+  created () {
+    // let app = getApp()
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .content {
+    .img {
+      width: 100rpx;
+      height: 100rpx;
+      border-radius: 50%;
+    }
+  }
+
+  .bg {
+    background: url("../../assets/img/root_demo_demo.jpeg") no-repeat center / 100% 100%;
+    width: 200rpx;
+    height: 200rpx;
+  }
+</style>
